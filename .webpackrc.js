0f7582d220879cc5d43c1a15f743fc8d303d1b39
yangfanvoid <908993023@qq.com>
2018-04-29 19:45:47 +0800

    .webpack错误

--- .webpackrc.js
+++ .webpackrc.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 export default {
   entry: 'src/index.js',
+extraBabelPlugins: [
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
   ],
   env: {
